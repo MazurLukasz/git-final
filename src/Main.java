ae5dda4c6858ec4a0b12643e2054670ad9cdf64b
mazur <mazuroo448@gmail.com>
2018-11-25 13:41:25 +0100

    od 10 do 0

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("GIT! v2");
+
+        System.out.println("GIT!");
+
+        System.out.println("10");
+        System.out.println("8");
+        System.out.println("6");
+        System.out.println("4");
+        System.out.println("2");
+        System.out.println("0");
     }
 }
